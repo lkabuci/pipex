2995147b826b4db30ce32c07bb2f20ed27eaf4fe
redouane <elkaboussi@pm.me>
2022-12-05 19:26:22 +0100

    buggy

--- bonus/main_bonus.c
+++ bonus/main_bonus.c
@@ -12,10 +12,33 @@
 
 #include "pipex_bonus.h"
 
+pid_t	*get_pids(t_params p)
+{
+	pid_t	*arr_of_pids;
+	int		i;
+
+	i = 0;
+	if (!p.cmd)
+		return ((pid_t *)0);
+	arr_of_pids = (pid_t) malloc(sizeof(pid_t) * (p.main.ac - 1));
+	if (!arr_of_pids)
+		return ((pid_t *)0);
+	while (p.cmd)
+	{
+		arr_of_pids[i] = i;
+		i++;
+		p.cmd = p.cmd->next;
+	}
+	arr_of_pids[i] = 0;
+	return (arr_of_pids);
+}
+
 int	main(int argc, char **argv, char **envp)
 {
+	pid_t		*arr_of_pids;
 	t_main		main;
 	t_params	p;
+	int			i;
 
 	main.ac = argc;
 	main.av = argv;
@@ -23,23 +46,41 @@ int	main(int argc, char **argv, char **envp)
 	p.main = main;
 	p.cmd = NULL;
 	parse(&p);
-	printf("%s\n", p.file.infile);
-	printf("%s\n", p.file.outfile);
-	int i = -1;
-	while (p.cmd != NULL)
+
+	i = 0;
+	arr_of_pids = get_pids(p);
+	while (p.cmd, i)
 	{
-		i = -1;
-		printf("cmd: %s\n", p.cmd->cmd);
-		while (p.cmd->args[++i] != NULL)
+		arr_of_pids[i] = fork();
+		
+		if (arr_of_pids[i] == 0)
 		{
-			printf("args: %s\t", p.cmd->args[i]);
+			//FIXME - HERE
+			dup2(p.cmd->fd[0]);
 		}
-		printf("\n");
-		printf("path: %s\n", p.cmd->path);
-		printf("========================\n");
+		
 		p.cmd = p.cmd->next;
-		sleep(1);
+		i++;
 	}
 	
+	/* print params
+		* int i = -1;
+		* printf("%s\n", p.file.infile);
+		* printf("%s\n", p.file.outfile);
+		* while (p.cmd != NULL)
+		* {
+		* 	i = -1;
+		* 	printf("cmd: %s\n", p.cmd->cmd);
+		* 	while (p.cmd->args[++i] != NULL)
+		* 	{
+		* 		printf("args: %s\t", p.cmd->args[i]);
+		* 	}
+		* 	printf("\n");
+		* 	printf("path: %s\n", p.cmd->path);
+		* 	printf("========================\n");
+		* 	p.cmd = p.cmd->next;
+		* 	sleep(1);
+		* }
+	*/
 	return 0;
 }
